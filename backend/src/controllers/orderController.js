71511e00693f89de10558e7ce8ff812c061cf674
Sai Bharath Vemula <bharathvemula118@gmail.com>
2025-09-18 00:22:49 +0530

    feat: add API viewing and updating orders

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -35,4 +35,29 @@ const getMyOrders=async(req,res)=>{
     }
 };
 
-module.exports={createOrder,getMyOrders};
+const getAllOrders=async(req,res)=>{
+    try{
+        const orders=await Order.find({}).populate('user','id name email');
+        res.json(orders);
+    } catch(error){
+        res.status(500).json({message:'Server Error', error: error.message})
+    }
+};
+
+const updateOrderStatus=async(req,res)=>{
+    try{
+        const order=await Order.findById(req.params.id);
+
+        if(order){
+            order.orderStatus=req.body.status || order.orderStatus;
+            const updatedOrder=await order.save();
+            res.json(updatedOrder);
+        }else{
+            res.status(404).json({message:'Order not found'});
+        }
+    }catch(error){
+        res.status(500).json({message:'Serve Error',error:error.message})
+    }
+};
+
+module.exports={createOrder,getMyOrders, getAllOrders, updateOrderStatus};
--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,10 +1,13 @@
 const express=require('express');
-const {createOrder,getMyOrders}=require('../controllers/orderController');
-const {protect}=require('../middleware/authMiddleware');
+const {createOrder,getMyOrders, getAllOrders, updateOrderStatus}=require('../controllers/orderController');
+const {protect, admin}=require('../middleware/authMiddleware');
 
 const router=express.Router();
 
 router.route('/').post(protect,createOrder);
 router.route('/myorders').get(protect,getMyOrders);
+
+router.route('/').get(protect,admin,getAllOrders);
+router.route('/:id/status').put(protect,admin,updateOrderStatus);
 
 module.exports=router;
