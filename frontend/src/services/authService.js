de7df1b9088acda64c2becb60fba082cffeb1ef1
Sai Bharath Vemula <bharathvemula118@gmail.com>
2025-09-18 20:08:48 +0530

    feat:fix a minor in authService in frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,14 +1,14 @@
 import axios from '../api/axios.js';
 
-const API_URL = '/restaurants/';
+// const API_URL = '/restaurants/';
 
 const register=async (userData)=>{
-    const response=await axios.post(API_URL + 'register', userData);
+    const response=await axios.post('/auth/register', userData);
     return response.data
 };
 
 const login = async (userData)=>{
-    const response=await axios.post(API_URL+'login',userData);
+    const response=await axios.post('/auth/login',userData);
     if(response.data){
         localStorage.setItem('user',JSON.stringify(response.data));
     }
